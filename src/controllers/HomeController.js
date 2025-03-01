38077e8b7f41bf0edf260e8883ab1ab593798c6a
Mateus Caetano <mateuscaetano952@gmail.com>
2025-03-01 08:45:49 -0300

    rota home funcionando

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,5 +1,9 @@
-exports.index = (req, res) => {
-  res.json({
-    Messagem: 'Ola',
-  });
-};
+class HomeController {
+  index(req, res) {
+    res.json({
+      messagem: 'Ola tudo bem',
+    });
+  }
+}
+
+export default new HomeController();
