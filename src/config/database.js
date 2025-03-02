fa21f28b06e55170c5476ff388a764aed83a7e7f
Mateus Caetano <mateuscaetano952@gmail.com>
2025-03-02 17:20:13 -0300

    sequelize configs

--- src/config/database.js
+++ src/config/database.js
@@ -1,21 +1,21 @@
-require('dotenv').config()
+require('dotenv').config();
 
 module.exports = {
-    dialect: 'mariadb',
-    host:
-    port:
-    username:
-    password:
-    database:
-    dafine: {
-        timestamps: true,
-        underscored: true,
-        underscoreAll: true,
-        'createAt': 'created_at',
-        'updateAt': 'update_at'
-    },
-    dialectOptions: {
-        timezone: 'America/Sao_Paulo'
-    },
-    timezone: 'America/Sao_Paulo'
-}
+  dialect: 'mariadb',
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PORT,
+  database: process.env.DATABASE,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoreAll: true,
+    createAt: 'created_at',
+    updateAt: 'update_at',
+  },
+  dialectOptions: {
+    timezone: 'America/Sao_Paulo',
+  },
+  timezone: 'America/Sao_Paulo',
+};
