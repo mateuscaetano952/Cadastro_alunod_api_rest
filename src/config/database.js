947b8603c46dfbdfcb4e4f485101ceaad8fcd01c
Mateus Caetano <mateuscaetano952@gmail.com>
2025-03-03 09:54:35 -0300

    migração concluida

--- src/config/database.js
+++ src/config/database.js
@@ -5,7 +5,7 @@ module.exports = {
   host: process.env.DATABASE_HOST,
   port: process.env.DATABASE_PORT,
   username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PORT,
+  password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE,
   define: {
     timestamps: true,
---
+++ src/databse/migrations/20250302203242-alunos.js
@@ -0,0 +1,41 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('alunos', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      nome: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      sobrenome: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      idade: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      create_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      update_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('alunos');
+  },
+};
