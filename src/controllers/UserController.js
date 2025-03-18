c5874f90a829023d1462cc0fdca8b100640ca8fc
Mateus Caetano <mateuscaetano952@gmail.com>
2025-03-18 08:37:18 -0300

    crud completo

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,17 +1,102 @@
+import { json } from 'sequelize';
 import User from '../models/User';
 
 class HomeController {
-  async index(req, res) {
+  async store(req, res) {
    try{
-    console.log('User route::' + req.body)
     const novoUser= await User.create(req.body);
-      res.json(novoALuno);
+      return res.json(novoALuno);
    }catch(e){
     res.status(400).json({
       errors: e.errors ? e.errors.map((err) => err.message) : e
     })
    }
   }
+
+  async home(req, res) {
+    try{
+      const users = await User.findAll();
+      return res.json(users);
+    }catch(e){
+      return res.json(null);
+    }
+  }
+
+  async show(req, res) {
+    try{
+      if(!req.params.id){
+        res.status(400).json({
+          errors: ['ID não enviado']
+        })
+      }
+  
+      const user = await User.findByPk(req.params.id)  
+      if(!user){
+        res.status(400).json({
+          errors: ['Usuario não existe']
+        })
+      } 
+  
+      return res.status(200).json(user)
+    }catch(e){
+      res.status(400).json({
+      errors: e.errors ? e.errors.map((err) => err.message) : e
+      })
+    }
+  }
+
+  async update(req, res){
+    try{
+      if(!req.params.id){
+        res.status(400).json({
+          errors: ['ID não enviado']
+        })
+      }
+  
+      const user = await User.findByPk(req.params.id)  
+      if(!user){
+        res.status(400).json({
+          errors: ['Usuario não existe']
+        })
+      } 
+      
+      const novosDados = await user.update(req.body);
+
+      
+
+      return res.status(200).json(novosDados)
+    }catch(e){
+      res.status(400).json({
+      errors: e.errors ? e.errors.map((err) => err.message) : e
+      })
+    }
+  }
+
+  async delete(req, res){
+    try{
+      if(!req.params.id){
+        res.status(400).json({
+          errors: ['ID não enviado']
+        })
+      }
+  
+      const user = await User.findByPk(req.params.id)  
+      if(!user){
+        res.status(400).json({
+          errors: ['Usuario não existe']
+        })
+      } 
+
+      user.destroy();
+  
+      return res.status(200).json(user)
+    }catch(e){
+      res.status(400).json({
+      errors: e.errors ? e.errors.map((err) => err.message) : e
+      })
+    }
+  }
 }
+
 
 export default new HomeController();
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,6 +3,10 @@ import userController from '../controllers/UserController';
 
 const router = Router();
 
-router.get('/user', userController.index);
+router.get('/user', userController.home);
+router.get('/user/:id', userController.show);
+router.post('/user', userController.store);
+router.put('/user/:id', userController.update);
+router.delete('/user/:id', userController.delete);
 
 export default router;
